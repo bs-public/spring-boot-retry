fd6509ec294e4f512d3028aa996112af39d8ae04
bs <26665808+bs-public@users.noreply.github.com>
2025-06-07 18:21:26 +1000

    Initial commit

---
+++ src/main/java/com/app/SpringBootRetryApplication.java
@@ -0,0 +1,12 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRetryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootRetryApplication.class, args);
+  }
+}
